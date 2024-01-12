51d3e70235a563130ab8efdf631d13ed14b2ed6f
Javlon_mac <richk880@gmail.com>
2024-01-12 15:59:37 +0500

    test from finance

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from random import shuffle
 
 
 def read_file():
-    with open('taxation.txt', 'r') as file:
+    with open('finance.txt', 'r') as file:
         reader = file.read()
 
     return str(reader)
@@ -12,29 +12,32 @@ def read_file():
 
 def clean_data(data):
     data = data.replace('#', '').replace('\n', '')
-    q_list = data.split('+++++')
+    q_list = data.split('++++')
 
     q_dict = {'questions': {}}
 
     for question in q_list[:-1]:
-        tem_format = question.split('====')
-        main_text = tem_format[0]
-        v1 = tem_format[1]
-        v2 = tem_format[2]
-        v3 = tem_format[3]
-        v4 = tem_format[4]
-        answer = tem_format[1]
-        variants = [v1, v2, v3, v4]
-        shuffle(variants)
+        try:
+            tem_format = question.split('====')
+            print(tem_format)
+            main_text = tem_format[0]
+            v1 = tem_format[1]
+            v2 = tem_format[2]
+            v3 = tem_format[3]
+            v4 = tem_format[4]
+            answer = tem_format[1]
+            variants = [v1, v2, v3, v4]
+            shuffle(variants)
 
-        q_dict.get('questions').update({main_text: {'variants': variants,
-                                                    'answer': variants.index(answer)}})
-
+            q_dict.get('questions').update({main_text: {'variants': variants,
+                                                        'answer': variants.index(answer)}})
+        except:
+            continue
     return q_dict
 
 
 def make_json(clean_data):
-    with open('taxation.json', 'w') as file:
+    with open('finance.json', 'w') as file:
         data = json.dumps(clean_data)
         file.write(data)
 
@@ -42,7 +45,7 @@ def make_json(clean_data):
 
 
 def get_json():
-    with open('taxation.json', 'rb') as file:
+    with open('finance.json', 'rb') as file:
         data = json.loads(file.read())
 
     return data
